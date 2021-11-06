a011336c7ad0301cf5cb5cf7f533c80139c33c4a
renehorstmann <r.horsty@gmail.com>
2021-11-06 08:14:03 +0100

    gpush

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
+#include <SDL2/SDL.h>
 #include "rhc/rhc_impl.h"
 
-int main() {
-    puts("Hello World");
+int main(int argc, char **argv) {
+    SocketServer server = socketserver_new("0.0.0.0", 10000);
+    for(;;) {
+        Socket *client = socketserver_accept(&server);
+        
+        Stream_i s = socket_get_stream(client);
+        
+        stream_write_msg(s, "Hi", 3);
+        
+        socket_kill(&client);
+    }
 }
-
