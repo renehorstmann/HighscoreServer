18912d1461336d299c8f33cfba29d313b441e323
renehorstmann <r.horsty@gmail.com>
2021-11-12 11:04:37 +0100

    init

--- src/highscore.c
+++ src/highscore.c
@@ -1,14 +1,34 @@
 #include "rhc/string.h"
 #include "highscore.h"
 
+#define TYPE HighscoreEntry
+#define CLASS HE_Array
+#define FN_NAME he_array
+#include "rhc/dynarray.h"
 
+
+//
+// private
+//
+
+static Str_s eat_entry(Str_s s, HighscoreEntry *opt_entry) {
+
+    return s;
+}
 
 //
 // protected
 //
 
 Highscore highscore_decode(Str_s msg) {
-
+    HE_Array array = he_array_new(8);
+    while(!str_empty(msg)) {
+        Str_s line;
+        msg = str_eat_until(msg, '\n', &line);
+        msg = str_eat(msg, 1);  // newline
+        line = str_strip(line, ' ');
+        // todo
+    }
 }
 
 String highscore_encode(Highscore self) {
